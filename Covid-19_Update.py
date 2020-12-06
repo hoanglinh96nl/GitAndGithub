b921c5ec9dcda65eb8eb3249ea863836667a1bda
Truong Hoang Linh <hoanglinh96nl@gmail.com>
2020-12-06 21:27:42 +0800

    Add a new program (Covid-19 status update)

---
+++ Covid-19_Update.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec  6 20:55:20 2020
+
+@author: Truong Hoang Linh
+"""
+
+import requests
+from win10toast import ToastNotifier
+import json
+import time
+
+def update():
+    r = requests.get('https://coronavirus-19-api.herokuapp.com/all')
+    data = r.json()
+    text = f'Case: {data["cases"]} \nDeaths: {data["deaths"]} \nRecovered: {data["recovered"]}'
+    
+    while True:
+        t = ToastNotifier()
+        t.show_toast('Covid-19 Update', text, duration=20)
+        time.sleep(60)
+        
+update()
